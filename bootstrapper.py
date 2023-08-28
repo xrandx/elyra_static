bd364d12aef6097681905457a0163f7d2982eee6
Hongjian Tang <kevtyle@hotmail.com>
2023-08-28 17:02:50 +0800

    Update bootstrapper.py

--- bootstrapper.py
+++ bootstrapper.py
@@ -585,7 +585,8 @@ class OpUtil(object):
                 to_install_list = ["-i", "http://pypi.douban.com/simple/", "--trusted-host", "pypi.douban.com", "--trusted-host", "files.pythonhosted.org"] + to_install_list
             print("line 586")
             subprocess.run([sys.executable, "-m", "pip", "install"] + to_install_list, check=True)
-
+            print([sys.executable, "-m", "pip", "install"] + to_install_list)
+        
         if user_volume_path:
             os.environ["PIP_CONFIG_FILE"] = f"{user_volume_path}/pip.conf"
 
